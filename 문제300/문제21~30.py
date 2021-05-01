d4e76457e5835fc9d1e57a2b3291120853c2a312
oijmark7 <oijmark7@gmail.com>
2021-05-01 17:59:50 +0900

    1

---
+++ 문제300/문제21~30.py
@@ -0,0 +1,22 @@
+#문제21
+letters='python'
+print(letters[0],letters[2])
+#문제22
+license_plate="24가 2210"
+print(license_plate[-4:])#뒤에서부터 4개
+print(license_plate[4:8])
+#문제23
+string="홀짝홀짝홀짝"
+print(string[0:6:2])
+#문제24
+string="PYTHON"
+print(string[::-1])
+#문제25
+phone_number ="010-1111-2222"
+phone_number1=phone_number.replace("-"," ")
+print(phone_number1)
+#문제26
+phone_number ="010-1111-2222"
+phone_number1=phone_number.replace("-","")
+print(phone_number1)
+
