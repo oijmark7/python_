e3a613e753d09f4c3c180eb329a40be96389e3ba
oijmark7 <oijmark7@gmail.com>
2021-05-01 16:50:04 +0900

    1

---
+++ 문제300/문제11~20.py
@@ -0,0 +1,41 @@
+#문제11
+삼성전자=50000
+총평가금액= 삼성전자*10#10주
+print(총평가금액)
+#문제12
+시가총액=290000000000000
+현재가=50000
+PER=15.79
+#문제13
+s="hello"
+t="python"
+print(s,end='');print(t)
+#문제14
+print(2+2*3)
+#문제15
+a=128
+print(type(a))
+    #int:정수 #float:실수
+    #str:문자
+#문제16
+num_str="720"
+num_int= int(num_str)
+print(num_int)
+#문제17
+num=100
+num_str= str(num)
+print(num_str)
+#문제18
+문자열="15.79"
+실수형=float(문자열)
+print(실수형)
+#문제19
+year="2020"
+print(int("2020"))
+print(int("2020")+1)
+print(int("2020")+2)
+#문제20
+에어컨=48584
+무이자=36
+총금액=에어컨*무이자
+print(총금액)
--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -1,5 +1,23 @@
 
 
 #문제 1
-
+print("Hello World")
 #문제 2
+print("Mary's cosmetics")
+#문제 3
+print('신씨가 소리질렀다,"도둑이야".')
+#문제 4
+print('"C:\Windows"')
+#문제5
+print("안녕하세요\n만나서\t\t반갑습니다")
+#\n=엔터 #\t=탭
+#문제6
+print("오늘은","일요일")
+#문제7
+print("naver;kakao;sk;samsung")
+#문제8
+print("naver/kakao/sk/samsung")
+#문제9
+print("first",end='');print("second")
+#문제10
+print("5/3")
